60f9d0bb060118507137cac3883c18992bd4f7cd
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-21 15:40:37 -0700

    Added test for existence of Enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,1 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require 'pry'
 
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_existence_of
+    asssert_instance_of Enigma, @enigma
+  end
+end
