4923ef4d14c9ede5f0d29bdb62f41649f379616f
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-22 16:08:34 -0700

    Added final shifts method

--- test/key_test.rb
+++ test/key_test.rb
@@ -14,14 +14,14 @@ class KeyTest < Minitest::Test
 
   def test_keys_are_seperated_from_generated_number
     expected = {
-      A_key: 02,
+      A_key: Integer,
       B_key: 27,
       C_key: 71,
       D_key: 15
     }
     assert_equal expected, @key.keys
   end
-  
+
   def test_getting_squared_value_of_the_date
     assert_equal "1672401025", @key.date_squared
   end
@@ -34,5 +34,15 @@ class KeyTest < Minitest::Test
       D_offset: 5
     }
     assert_equal expected, @key.offsets
+  end
+
+  def test_final_shifts_for_encryption
+    expected = {
+      A: 3,
+      B: 27,
+      C: 73,
+      D: 20
+    }
+    assert_equal expected, @key.final_shifts
   end
 end
