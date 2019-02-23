b494c99ad6505089a3caf8facd35dc0d560c4913
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-22 17:57:37 -0700

    Added test for letter shift

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/encryption'
+require './lib/key'
 require 'pry'
 
 class EncryptionTest < Minitest::Test
@@ -16,5 +17,9 @@ class EncryptionTest < Minitest::Test
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
      assert_equal expected, @encryption.character_set
+  end
+
+  def test_change_of_letter_from_character_set_array
+    assert_equal "e" , @encryption.letter_shift
   end
 end
