200be92121db8735ea86b63f0c4e6cc1b6a753d1
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-22 17:58:14 -0700

    Added method for letter shift

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,4 +2,10 @@ class Encryption
   def character_set
     ("a".."z").to_a << " "
   end
+
+  def letter_shift
+    number = 4
+    letter = "a"
+    character_set[character_set.index(letter)+number]
+  end
 end
