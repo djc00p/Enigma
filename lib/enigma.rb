fb231b31a3a4ced9a1070b8b809754ba6702e97c
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-24 19:38:22 -0700

    Added encrypt & decrypt method to enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,14 @@
 class Enigma
 
+  def encrypt(message, key, date)
+    {
+      encryption: Encryption.encrypt_message(message),
+      key: key,
+      date: date
+    }
+  end
 
+  def decrypt(encrypted_message, key, date)
+
+  end
 end
